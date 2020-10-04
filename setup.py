1c073db5f98322444033ddb6d001bb931c363e18
swapan-linux <sdbit46@gmail.com>
2020-10-04 12:58:20 +0530

    Added github-link at setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,8 @@ from setuptools import setup
 setup( name="pythonc",
 description="this give a command pythonc to compile any python module",
 version="1.1.0",
-author="swapan",
+author="swapankumarDas",
+url="https://github.com/sdbit04/pythonc.git",
 packages=["pythonc"],
 entry_points={'console_scripts': [
     'pythonc=pythonc.pythonc:main_method',
